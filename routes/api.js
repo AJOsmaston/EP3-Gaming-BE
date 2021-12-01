ec68a052e819a56f21f4536111668bd23329782e
Kostas Ntanos <kostas@ntanos.co.uk>
2021-12-01 14:38:08 +0000

    SIGNUP ROUTE RECEIVES FROM FE
    The signup form on front end sends username and password to the signup route.
    Next step to put that in the database.

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const Game = require("../src/lib/game");
 const router = express.Router();
@@ -6,6 +7,14 @@ router.get("/", (req, res) => {
   res.send("Hello World")
 });
 
+// USER ROUTES ----------------------------------
+
+router.post("/signup", (req, res, next) => {
+  console.log(req.body.username); // Now the username and password from the body need to go to the db
+  res.json(req.body); // What do we return?
+})
+
+// GAME ROUTES ----------------------------------
 router.get("/startGame", (req, res, next) => {
   newGame = new Game;
 
