c5b688a8a1c95960f62187c61182e8eb59b7cefb
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-12-02 17:25:10 +0000

    stuff

--- routes/api.js
+++ routes/api.js
@@ -1,89 +1,84 @@
 const express = require("express");
 const Game = require("../src/lib/game");
 const router = express.Router();
-const Scoreboard = require('../models/scoreboard')
-const UserModel = require('../models/user')
-const passport = require('passport');
-
+const Scoreboard = require("../models/scoreboard");
+const UserModel = require("../models/user");
+const passport = require("passport");
 
 router.get("/", (req, res) => {
-  res.send("Hello World")
+  res.send("Hello World");
 });
 
 // USER ROUTES ----------------------------------
 
 router.post("/signup", (req, res, next) => {
-  UserModel.register(new Account({ username : req.body.username, password : req.body.password }), req.body.password)
-})
+  UserModel.register(
+    new UserModel({ username: req.body.username, password: req.body.password }),
+    req.body.password
+  );
+});
 
-router.post("/login", 
-passport.authenticate('local', 
-{ 
-  successRedirect: '/start-game',
-  failureRedirect: '/login' 
-}), (req, res, next) => {
-  req.session.save((err) => {
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+  }),
+  (req, res, next) => {
+    console.log("login successfull");
+    req.session.save((err) => {
       if (err) {
-          return next(err);
+        return next(err);
       }
-      res.redirect('/');
-  })
-})
+      res.redirect("/");
+    });
+  }
+);
 
-router.get('/logout', (req, res, next) => {
+router.get("/logout", (req, res, next) => {
   req.logout();
   req.session.save((err) => {
-      if (err) {
-          return next(err);
-      }
-      res.redirect('/');
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
-})
+});
 
 // GAME ROUTES ----------------------------------
 
 router.get("/start-game", (req, res, next) => {
-  newGame = new Game;
+  newGame = new Game();
 
-  let score = newGame.score
-  let health = newGame.health
+  let score = newGame.score;
+  let health = newGame.health;
   let isDead = newGame.checkDead();
-  
-  res.status(200).json({ score: score, health: health, isDead: isDead })
-})
+
+  res.status(200).json({ score: score, health: health, isDead: isDead });
+});
 
 router.get("/turn", (req, res, next) => {
   newGame.attack();
   newGame.takeDamage();
 
-  let score = newGame.score
-  let health = newGame.health
+  let score = newGame.score;
+  let health = newGame.health;
   let isDead = newGame.checkDead();
 
-  res.status(200).json({ score: score, health: health, isDead: isDead })
-})
+  res.status(200).json({ score: score, health: health, isDead: isDead });
+});
 
 router.get("/commit-score", (req, res, next) => {
-
-  res.send('attempting to post something to mongodb');
+  res.send("attempting to post something to mongodb");
 
   const addScore = async () => {
-    
-    const newScore = new Scoreboard({ score: newGame.score })
-    await newScore.save()
-    console.log(`saved ${newScore}`)
-    
-   
+    const newScore = new Scoreboard({ score: newGame.score });
+    await newScore.save();
+    console.log(`saved ${newScore}`);
   };
 
-  addScore()
-  
-
- 
+  addScore();
 
   //createAndSaveScoreboard()
-
-})
-
+});
 
 module.exports = router;
