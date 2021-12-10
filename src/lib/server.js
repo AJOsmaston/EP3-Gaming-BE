21c62e9646f283021a4d27e03d0a70fdc5bd78e5
AJOsmaston <dajoxx@gmail.com>
2021-12-10 14:23:28 +0000

    changed heroku deploy

--- src/lib/server.js
+++ src/lib/server.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://makersep3gamefrontend.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', 'https://life-of-bernard.herokuapp.com');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
