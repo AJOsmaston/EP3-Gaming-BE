43f09977903d87c37a39f772794cbd7340c81f42
AJOsmaston <dajoxx@gmail.com>
2022-01-06 18:16:34 +0000

    added headers to both if and else if, added all sections of session to if and else if

--- src/lib/server.js
+++ src/lib/server.js
@@ -8,28 +8,23 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoDBSession = require('connect-mongodb-session')(session);
 
-
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 if (process.env.ENVIRONMENT === 'DEPLOY') {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://life-of-bernard.herokuapp.com');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', true);
     next();
   });
 } else if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-} else {
-  app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-}
+};
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -42,13 +37,17 @@ const store = new MongoDBSession({
 if (process.env.ENVIRONMENT === 'DEPLOY') {
   app.enable('trust proxy');
   app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
     proxy : true,
     cookie: {
       secure : true,
       sameSite: 'none',
     }
   }));
-} else {
+} else if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
   app.use(session({
     secret: process.env.SECRET,
     resave: false,
